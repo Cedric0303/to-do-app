7873ee72fc45a9d91806861f77d4b0f016999828
Jun Li Chen <chenjunli7@gmail.com>
2023-10-22 23:37:52 +1100

    Add test add empty activity

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -10,7 +10,13 @@ describe("E2E test", () => {
         cy.get(".submitButton > button").click();
         cy.contains(activityText);
     });
-    it("Test 2: Add multiple activity", () => {
+
+    it("Test 2: Add empty activity", () => {
+        cy.get(".submitButton > button").click();
+        cy.get(".errorMessage");
+    });
+
+    it("Test 3: Add multiple activity", () => {
         // Add 2 activities back to back
         const activityText1 = "Finish testing frontend 0303";
         const activityText2 = "Finish E2E testing frontend 0303";
@@ -22,7 +28,8 @@ describe("E2E test", () => {
         cy.contains(activityText1);
         cy.contains(activityText2);
     });
-    it("Test 3: Toggle activity done", () => {
+
+    it("Test 4: Toggle activity done", () => {
         const activityText = "Toggle activity done state 0303";
         cy.get(".textInput").type(activityText);
         cy.get(".submitButton > button").click();
@@ -33,7 +40,8 @@ describe("E2E test", () => {
         cy.get(".doneCell").click();
         cy.get(".doneCell").not("✅");
     });
-    it("Test 4: Delete an activity", () => {
+
+    it("Test 5: Delete an activity", () => {
         const activityText = "Delete activity 0303";
         cy.get(".textInput").type(activityText);
         cy.get(".submitButton > button").click();
@@ -41,7 +49,8 @@ describe("E2E test", () => {
         cy.get(".deleteButton").click();
         cy.get("body").not("Delete activity");
     });
-    it("Test 5: Edit an activity", () => {
+
+    it("Test 6: Edit an activity", () => {
         // Edit an activity, submit it, edit again, and cancel it
         const activityText = "Edit activity 0303";
         const editText = "Edit activity 0303";
@@ -61,7 +70,7 @@ describe("E2E test", () => {
         cy.contains(editText);
     });
 
-    it("Test 1: Delete all activities", () => {
+    it("Test 7: Delete all activities", () => {
         // Create 10 activities, then press delete all button
         const activityText = "Delete all activities 0303";
         for (let i = 0; i < 10; i++) {
